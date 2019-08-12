e8b2d16040734661d485cb0f7aff9298da545356
Chad Kirby <ckirby@biddle.com>
2019-08-12 15:26:30 -0700

    Added some basic client-side retry logic

--- src/DynHosts.Client/Program.cs
+++ src/DynHosts.Client/Program.cs
@@ -4,6 +4,8 @@ using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,64 +17,89 @@ namespace DynHosts.Client
 
         public static async Task<int> Main(string[] args)
         {
-            var app = new CommandLineApplication
-            {
-                Name = "dhclient",
-                Description = "DynHosts Client"
-            };
-
-            app.HelpOption("-?|-h|--help");
-
-            var remoteHostOption = app.Option("-r|--remote-host <URL>",
-                    "The URL of the DynHosts Server to connect to.",
-                    CommandOptionType.SingleValue);
-
-            var hostnameOption = app.Option("-h|--hostname <NAME>",
-                    "The hostname to modify in the HOSTS file.",
-                    CommandOptionType.SingleValue);
-
-            var ipsOption = app.Option("-i|--ip <IP>",
-                    "The IP address(es) to set the hostname value to in the HOSTS file. Specify multiple times for multiple IP adresses e.g. --ip 127.0.0.1 --ip ::1",
-                    CommandOptionType.MultipleValue);
-
-            app.OnExecuteAsync(async (cancellationToken) =>
-            {
-                if (remoteHostOption.HasValue() && hostnameOption.HasValue() && ipsOption.HasValue())
+            using (
+                var app = new CommandLineApplication
                 {
-                    s_httpClient.BaseAddress = new Uri(remoteHostOption.Value());
+                    Name = "dhclient",
+                    Description = "DynHosts Client"
+                }
+            )
+            {
+                app.HelpOption("-?|-h|--help");
 
-                    string remoteHost = remoteHostOption.Value();
-                    string hostname = hostnameOption.Value();
+                var remoteHostOption = app.Option("-r|--remote-host <URL>",
+                        "The URL of the DynHosts Server to connect to.",
+                        CommandOptionType.SingleValue);
 
-                    Console.Write($"Requesting server at {remoteHost} update host \"{hostname}\"... ");
-                    var postContent = new StringContent(JsonConvert.SerializeObject(new { host = hostname, ipAddresses = ipsOption.Values.ToArray() }), Encoding.UTF8, "application/json");
+                var hostnameOption = app.Option("-h|--hostname <NAME>",
+                        "The hostname to modify in the HOSTS file.",
+                        CommandOptionType.SingleValue);
 
-                    using (var response = await s_httpClient.PutAsync("/api/hosts/" + hostname, postContent))
+                var ipsOption = app.Option("-i|--ip <IP>",
+                        "The IP address(es) to set the hostname value to in the HOSTS file. Specify multiple times for multiple IP adresses e.g. --ip 127.0.0.1 --ip ::1",
+                        CommandOptionType.MultipleValue);
+
+                app.OnExecuteAsync(async (cancellationToken) =>
+                {
+                    if (remoteHostOption.HasValue() && hostnameOption.HasValue() && ipsOption.HasValue())
                     {
-                        if (new[] { HttpStatusCode.Created, HttpStatusCode.OK, HttpStatusCode.NoContent }.Contains(response.StatusCode))
-                        {
-                            Console.WriteLine("OK!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Failed.");
-                            Console.WriteLine($"{(int)response.StatusCode} - {response.ReasonPhrase}");
+                        s_httpClient.BaseAddress = new Uri(remoteHostOption.Value());
+                        s_httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
 
-                            string responseContent = await response.Content.ReadAsStringAsync();
-                            Console.WriteLine(responseContent);
+                        string remoteHost = remoteHostOption.Value();
+                        string hostname = hostnameOption.Value();
+
+                        Console.Write($"Requesting server at {remoteHost} update host \"{hostname}\"... ");
+                        var postContent = new StringContent(JsonConvert.SerializeObject(new { host = hostname, ipAddresses = ipsOption.Values.ToArray() }), Encoding.UTF8, "application/json");
+
+                        using (
+                            var response = await s_httpClient.PutAsync("/api/hosts/" + hostname, postContent)
+                                .ToObservable()
+                                .Select(r =>
+                                {
+                                    if (!r.IsSuccessStatusCode)
+                                    {
+                                        throw new HttpListenerException((int)r.StatusCode, r.ReasonPhrase);
+                                    }
+                                    return r;
+                                })
+                                .Retry(3)
+                                .Catch((Func<Exception, IObservable<HttpResponseMessage>>)(ex =>
+                                {
+                                    if (ex is HttpListenerException hle)
+                                    {
+                                        return Observable.Return(new HttpResponseMessage((HttpStatusCode)hle.ErrorCode) { ReasonPhrase = hle.Message });
+                                    }
+
+                                    throw ex;
+                                }))
+                        )
+                        {
+                            if (new[] { HttpStatusCode.Created, HttpStatusCode.OK, HttpStatusCode.NoContent }.Contains(response.StatusCode))
+                            {
+                                Console.WriteLine("OK!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Failed.");
+                                Console.WriteLine($"{(int)response.StatusCode} - {response.ReasonPhrase}");
+
+                                string responseContent = await response.Content.ReadAsStringAsync();
+                                Console.WriteLine(responseContent);
+                            }
                         }
+                        Console.WriteLine();
                     }
-                    Console.WriteLine();
-                }
-                else
-                {
-                    app.ShowHint();
-                }
+                    else
+                    {
+                        app.ShowHint();
+                    }
 
-                return 0;
-            });
+                    return 0;
+                });
 
-            return await app.ExecuteAsync(args);
+                return await app.ExecuteAsync(args);
+            }
         }
     }
 }
