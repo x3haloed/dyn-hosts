4ce7adee2f24dc05f3820a7a83b5fc5087d87ce4
Chad Kirby <ckirby@biddle.com>
2019-09-24 22:15:45 -0700

    Removed redubant publish batch file

--- publish.bat
+++
@@ -1,3 +0,0 @@
-dotnet publish -c Release -r win10-x64 /p:PublishSingleFile=true --self-contained true .\src\DynHosts.Client\DynHosts.Client.csproj
-dotnet publish -c Release -r win10-x86 /p:PublishSingleFile=true --self-contained true .\src\DynHosts.Client\DynHosts.Client.csproj
-dotnet publish -c Release -r win10-x64 --no-self-contained .\src\DynHosts.Server\DynHosts.Server.csproj
