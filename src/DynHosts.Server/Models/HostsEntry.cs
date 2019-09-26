faa1fe3e12553e49943616a92df8eaba14918071
Chad Kirby <ckirby@biddle.com>
2019-09-26 09:20:15 -0700

    Added HostsEntry and converter for DTO

---
+++ src/DynHosts.Server/Models/HostsEntry.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace DynHosts.Server.Models
+{
+    internal class HostsEntry
+    {
+        public HostsEntry(string host, IPAddress ipAddress)
+        {
+            Host = host;
+            IpAddress = ipAddress;
+        }
+
+        public HostsEntry(string host, string ipAddress)
+            : this(host, IPAddress.Parse(ipAddress)) { }
+
+        public string Host { get; set; }
+        public IPAddress IpAddress { get; set; }
+    }
+}
---
+++ src/DynHosts.Server/Models/HostsEntryConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DynHosts.Server.Models
+{
+    internal static class HostsEntryConverter
+    {
+        public static HostsEntryDto ToDto(IEnumerable<HostsEntry> hostsEntries)
+        {
+            HostsEntry[] hostsEntryArray = hostsEntries.ToArray();
+
+            // ensure that the hosts entries are all for the same host name
+            if (hostsEntryArray.Select(he => he.Host).Distinct(StringComparer.OrdinalIgnoreCase).Count() != 1)
+            {
+                throw new ArgumentException("expected one unique host name", nameof(hostsEntries));
+            }
+
+            var result = new HostsEntryDto
+            {
+                Host = hostsEntryArray[0].Host,
+                IpAddresses = hostsEntryArray.Select(he => he.IpAddress.ToString()).ToArray()
+            };
+
+            return result;
+        }
+
+        public static HostsEntryDto[] ToDtos(IEnumerable<HostsEntry> hostsEntries)
+        {
+            HostsEntryDto[] results = hostsEntries
+                .GroupBy(he => he.Host, StringComparer.OrdinalIgnoreCase)
+                .Select(eg => new HostsEntryDto
+                {
+                    Host = eg.Key,
+                    IpAddresses = eg.Select(he => he.IpAddress.ToString()).ToArray()
+                })
+                .ToArray();
+
+            return results;
+        }
+    }
+}
